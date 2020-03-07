ee8110e953ec3b241833f11d18a6f8ccec78dde4
BaronVonFroklok <thecheatisgod@gmail.com>
2020-03-07 01:33:36 -0700

    built last UI page TODO build controllers and finish all model methods

--- src/Model/Outsourced.java
+++ src/Model/Outsourced.java
@@ -2,8 +2,19 @@ package Model;
 
 import javafx.collections.ObservableList;
 
-public class Outsourced {
+public class Outsourced extends Part{
+    String companyName;
 
+    public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }
 
+    public String getCompanyName() {
+        return companyName;
+    }
 
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
